660ab8e7c12bfdcfb488c615f4988db865fa8558
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-08-07 09:55:08 +0600

    Add files via upload

---
+++ Java/CheckOddEven/src/checkoddeven/CheckOddEven.java
@@ -0,0 +1,29 @@
+package checkoddeven;
+import java.util.Scanner;
+
+public class CheckOddEven {
+    
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+
+        System.out.print("Enter a number: ");
+
+        int num = reader.nextInt();
+
+
+        if(num % 2 == 0)
+
+            System.out.println(num + " is even");
+
+        else
+
+            System.out.println(num + " is odd");
+
+         System.out.println("bye");
+
+    }
+        
+    }
+    
+

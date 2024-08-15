96322c227384293f31b5a501dd35132654b45993
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-08-15 23:41:52 +0600

    Add files via upload

---
+++ C++ Program/Default.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1, num2,total;
+    float num3, num4, decimal;
+    char ch;
+    ch='a';
+    num1 = 10;
+    num2 = 20;
+    num3 = 20.56;
+    num4 = 34.68;
+    decimal = num3 + num4;
+    total = num1+num2;
+    cout << "Total number is " << total << endl;
+    cout << "The character variable is " << ch  <<  endl;
+    cout << "The decimal number is  " << decimal << endl;
+    getch();
+}

3a0d3420f59a120458632098be8ee85c7ef0086c
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-08-21 22:27:46 +0600

    Add files via upload

---
+++ Java/Lab2/Ractangle.java
@@ -0,0 +1,18 @@
+
+package lab2;
+
+public class Ractangle {
+   public static void main(String[] args) 
+ {
+	double width,height;
+	width = 5.5;
+	height = 8.5;
+	float Area,Perameter;
+	Area=(float)(width*height);
+	Perameter=(float)(2*(width+height));
+	System.out.println("Area is =  "+Area);
+	System.out.println("Perameter is =  "+Perameter);
+	
+ }
+
+}

17952234a84f938354f6e9d095260e699e511630
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-08-26 23:41:29 +0600

    Add files via upload

---
+++ Java/Icecream.java
@@ -0,0 +1,8 @@
+
+public class Icecream {
+   // String  IcecreamType, IcecreamCompany;
+   //ouble IcecreamPrice;
+    public static void main(String[] args) {
+        System.out.println("my book");
+    }
+}

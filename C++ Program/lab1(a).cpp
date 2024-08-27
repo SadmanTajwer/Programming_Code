5d4459e73999fc5df7e9d15be8dad0b0091b2ef7
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-08-27 21:10:29 +0600

    Add files via upload

---
+++ C++ Program/lab1(a).cpp
@@ -0,0 +1,16 @@
+1. A grocery shop owner wants to store the information about the products that he has in the stock.
+A product has its unique id, name, brand name, type (for example food, cosmetic, electronic etc.),
+quantity and price of each unit. First you have to design a class with appropriate entry according
+to the problem specification. You should have come up with something like this:
+class product
+{
+Private:
+int id;
+string name;
+........
+};
+
+a. Now create an array of the objects and let the user to decide how many products info he/she
+wants to store. Then store the information using the created array of objects. After that
+display the name of the products and their prices whose prices are greater than 40.
+b. Repeat the process of (a) using dynamic memory allocation.

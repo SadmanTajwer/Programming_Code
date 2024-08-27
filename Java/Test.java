3b4f5b680c9a1a6cc95774621fc5b6fc63ab8dcd
Muhammad Sadman Tajwer <sadmantajwer990@gmail.com>
2024-08-27 21:00:59 +0600

    Add files via upload

---
+++ Java/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher();
+        teacher1.name = "Muhammad Sadman Tajwer";
+        teacher1.gender ="Male";
+        teacher1.age= 21;
+        teacher1.mobile = 1791203192;
+        teacher1.national_ID = 1909786759;
+        
+        System.out.println("Name = " +teacher1.name);
+        System.out.println("Gender = " +teacher1.gender);
+        System.out.println("Age = " +teacher1.age);
+        System.out.println("Mobile = " +teacher1.mobile);
+        System.out.println("National ID = " +teacher1.national_ID);
+    }
+}
